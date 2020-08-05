fa23975959a0141ee9fa08c7da4e16219bc4c9eb
stephendpmurphy <stephendpmurphy@msn.com>
2020-08-05 14:01:08 -0400

    Adding initial script and laying out things it needs to do

---
+++ setup.sh
@@ -0,0 +1,28 @@
+echo "######## Begining Linux Setup ########"
+
+# Add needed repositories
+# KICAD
+
+# Install all of my packages
+apt-get install git
+# Node
+# VS-Code
+# Spotify
+# Slack
+# Chrome
+# GNOME Tweaks
+# KiCad
+# Segger Tools
+# AVR Tools
+# ARM Compiler
+
+# Download & Apply Themes
+# Nord
+
+# Dot files
+# .bashrc (git branch info on end of line
+
+# End
+echo "######## Setup complete. Enjoy! ########"
+exit
+
