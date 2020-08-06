b676dc34c8d4edc3e8529d184eed38408653d483
stephenmurphy <stephendpmurphy@msn.com>
2020-08-06 14:16:09 -0400

    Adding install for VScode and the GNOME Extension for panel transparency

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# linux-setup
+# First time linux setup
 Shell script and related dot files for setting up my preferred linux environment
--- setup.sh
+++ setup.sh
@@ -11,34 +11,54 @@ bypass() {
 
 bypass
 
-echo "######## Begining Linux Setup ########"
-
-# Add needed repositories
-# KICAD
-
-echo "######## Updating repositories ########"
-sudo apt update
-
-# Install all of my packages and programs I commonly use
-# Node
+######## ADDING NEW APT REPOS ########
 # VScode
-## apt-get install code
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+# KiCAD
+
+sudo apt-get update
+
+######## PACKAGES AND APPLICATIONS ########
+
+# Update our current repos
+sudo apt-get -y upgrade
+
+# Node
+
+# VScode
+sudo apt-get install apt-transport-https
+sudo apt-get install code
+
 # Spotify
+
 # Slack
+
 # Chrome
+
 # GNOME Tweaks
 sudo apt-get install gnome-tweaks
+
 # GNOME Tweaks Extension - Dash to Panel
 sudo apt-get install gnome-shell-extension-dash-to-panel
+
 # KiCad
+
 # Segger Tools
+
 # AVR Tools
 sudo apt-get install gcc-avr avr-libc avrdude
+
 # ARM Compiler
+
 # Ruby
 sudo apt-get install ruby
+
 # XZ-Utils
 sudo apt-get install xz-utils
+
 # Ceedling unit test framework
 sudo gem install ceedling
 
@@ -53,15 +73,19 @@ sudo mv ./Nordic-darker /usr/share/themes/
 gsettings set org.gnome.desktop.interface gtk-theme "Nordic-darker"
 gsettings set org.gnome.desktop.wm.preferences theme "Nordic-darker"
 
-# Dot files
+# Download & Apply Transparent GNOME Extension
+git clone https://github.com/rockon999/dynamic-panel-transparency.git
+sudo mv ./dynamic-panel-transparency/dynamic-panel-transparency@rockon999.github.io ~/.local/share/gnome-shell/extensions/
+gnome-shell-extension-tool -e dynamic-panel-transparency
+
+######## CONFIG ########
+# Load our dconf DB
+dconf load / < gnome_settings.dconf
 # .bashrc (git branch info on end of line
 
-# Update all of our packages
-sudo apt-get -y upgrade
-
-# Clean up
+######## CLEANUP ########
 git clean -f -xd
+rm -r dynamic-panel-transparency
 
 echo "######## Setup complete. Enjoy! ########"
 exit
-
